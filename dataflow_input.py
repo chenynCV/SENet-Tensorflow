413c170a9b7dc8578760b2baba967a30800a0240
chenynCV <1756538768@qq.com>
2017-11-09 15:20:35 +0800

    Augmentor used in fb.resnet.torch, for BGR images in range [0,255].

--- dataflow_input.py
+++ dataflow_input.py
@@ -9,71 +9,78 @@ from tensorpack.dataflow import (
     BatchData, MultiThreadMapData, DataFlow)
 from IPython import embed
 
-# ========================================================== #
-# ├─ _random_crop()
-# ├─ _random_flip_leftright()
-# ├─ data_augmentation()
-# └─ color_preprocessing()
-# ========================================================== #
+class GoogleNetResize(imgaug.ImageAugmentor):
+    """
+    crop 8%~100% of the original image
+    See `Going Deeper with Convolutions` by Google.
+    """
+    def __init__(self, crop_area_fraction=0.08,
+                 aspect_ratio_low=0.75, aspect_ratio_high=1.333,
+                 target_shape=224):
+        self._init(locals())
 
-def _random_crop(batch, crop_shape, padding=None):
-    oshape = np.shape(batch[0])
+    def _augment(self, img, _):
+        h, w = img.shape[:2]
+        area = h * w
+        for _ in range(10):
+            targetArea = self.rng.uniform(self.crop_area_fraction, 1.0) * area
+            aspectR = self.rng.uniform(self.aspect_ratio_low, self.aspect_ratio_high)
+            ww = int(np.sqrt(targetArea * aspectR) + 0.5)
+            hh = int(np.sqrt(targetArea / aspectR) + 0.5)
+            if self.rng.uniform() < 0.5:
+                ww, hh = hh, ww
+            if hh <= h and ww <= w:
+                x1 = 0 if w == ww else self.rng.randint(0, w - ww)
+                y1 = 0 if h == hh else self.rng.randint(0, h - hh)
+                out = img[y1:y1 + hh, x1:x1 + ww]
+                out = cv2.resize(out, (self.target_shape, self.target_shape), interpolation=cv2.INTER_CUBIC)
+                return out
+        out = imgaug.ResizeShortestEdge(self.target_shape, interp=cv2.INTER_CUBIC).augment(img)
+        out = imgaug.CenterCrop(self.target_shape).augment(out)
+        return out
 
-    if padding:
-        oshape = (oshape[0] + 2 * padding, oshape[1] + 2 * padding)
-    new_batch = []
-    npad = ((padding, padding), (padding, padding), (0, 0))
-    for i in range(len(batch)):
-        new_batch.append(batch[i])
-        if padding:
-            new_batch[i] = np.lib.pad(batch[i], pad_width=npad,
-                                      mode='constant', constant_values=0)
-        nh = random.randint(0, oshape[0] - crop_shape[0])
-        nw = random.randint(0, oshape[1] - crop_shape[1])
-        new_batch[i] = new_batch[i][nh:nh + crop_shape[0],
-                       nw:nw + crop_shape[1]]
-    return new_batch
+def fbresnet_augmentor(isTrain, target_shape=224):
+    """
+    Augmentor used in fb.resnet.torch, for BGR images in range [0,255].
+    """
+    if isTrain:
+        augmentors = [
+            GoogleNetResize(target_shape=target_shape),
+            imgaug.RandomOrderAug(
+                [imgaug.BrightnessScale((0.6, 1.4), clip=False),
+                 imgaug.Contrast((0.6, 1.4), clip=False),
+                 imgaug.Saturation(0.4, rgb=False),
+                 # rgb-bgr conversion for the constants copied from fb.resnet.torch
+                 imgaug.Lighting(0.1,
+                                 eigval=np.asarray(
+                                     [0.2175, 0.0188, 0.0045][::-1]) * 255.0,
+                                 eigvec=np.array(
+                                     [[-0.5675, 0.7192, 0.4009],
+                                      [-0.5808, -0.0045, -0.8140],
+                                      [-0.5836, -0.6948, 0.4203]],
+                                     dtype='float32')[::-1, ::-1]
+                                 )]),
+            imgaug.Flip(horiz=True),
+        ]
+    else:
+        augmentors = [
+            imgaug.ResizeShortestEdge(int(256 / 224 * target_shape), cv2.INTER_CUBIC),
+            imgaug.CenterCrop((target_shape, target_shape)),
+        ]
+    return augmentors
 
-
-def _random_flip_leftright(batch):
-    for i in range(len(batch)):
-        if bool(random.getrandbits(1)):
-            batch[i] = np.fliplr(batch[i])
-    return batch
-
-
-def _random_multiply(batch, x, y):
-    for i in range(len(batch)):
-        if bool(random.getrandbits(1)):
-            batch[i] = random.uniform(x, y) * batch[i]
-            batch[i][batch[i] > 255] = 255
-            batch[i] = batch[i].astype(np.uint8)
-    return batch
-
-
-def color_preprocessing(x_train, x_test):
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train[:, :, :, 0] = (x_train[:, :, :, 0] - np.mean(x_train[:, :, :, 0])) / np.std(x_train[:, :, :, 0])
-    x_train[:, :, :, 1] = (x_train[:, :, :, 1] - np.mean(x_train[:, :, :, 1])) / np.std(x_train[:, :, :, 1])
-    x_train[:, :, :, 2] = (x_train[:, :, :, 2] - np.mean(x_train[:, :, :, 2])) / np.std(x_train[:, :, :, 2])
-
-    x_test[:, :, :, 0] = (x_test[:, :, :, 0] - np.mean(x_test[:, :, :, 0])) / np.std(x_test[:, :, :, 0])
-    x_test[:, :, :, 1] = (x_test[:, :, :, 1] - np.mean(x_test[:, :, :, 1])) / np.std(x_test[:, :, :, 1])
-    x_test[:, :, :, 2] = (x_test[:, :, :, 2] - np.mean(x_test[:, :, :, 2])) / np.std(x_test[:, :, :, 2])
-
-    return x_train, x_test
-
-
-def data_augmentation(batch, img_size=(224, 224)):
-    batch = _random_flip_leftright(batch)
-    # batch = _random_multiply(batch, 0.8, 1.2)
-    # batch = _random_crop(batch, img_size, padding=None)
-    return np.array(batch)
-
+def data_augmentation(im, augmentors):
+    """
+    See explanations in the tutorial:
+    http://tensorpack.readthedocs.io/en/latest/tutorial/efficient-dataflow.html
+    """
+    assert isinstance(augmentors, list)
+    aug = imgaug.AugmentorList(augmentors)
+    im = aug.augment(im)
+    return im
 
 class MyDataFlow(DataFlow):
-    def __init__(self, image_path, label_path, is_training=True, batch_size=8, img_size=(224, 224)):
+    def __init__(self, image_path, label_path, is_training=True, batch_size=64, img_size=224):
         # get all the image name and its label
         self.data_dict = {}
         with open(label_path, 'r') as f:
@@ -85,6 +92,7 @@ class MyDataFlow(DataFlow):
         self.is_training = is_training
         self.batch_size = batch_size
         self.img_size = img_size
+        self.augmentors = fbresnet_augmentor(isTrain=is_training, target_shape=img_size)
 
     def get_data(self):
         np.random.seed()
@@ -92,15 +100,15 @@ class MyDataFlow(DataFlow):
         img_data = []
         img_label = []
         for item in img_batch:
-            img_data.append(cv2.resize(cv2.imread(os.path.join(self.image_path, item)), self.img_size))
+            im = cv2.imread(os.path.join(self.image_path, item), cv2.IMREAD_COLOR)  
+            im = data_augmentation(im, self.augmentors)
+            img_data.append(im)
             img_label.append(self.data_dict[item])
-        if self.is_training:
-            img_data = data_augmentation(np.array(img_data), img_size=self.img_size)
         yield {'data': np.array(img_data), 'label': np.array(img_label)}
 
 
 class MyDataFlowEval(DataFlow):
-    def __init__(self, image_path, label_path, img_size=(224, 224)):
+    def __init__(self, image_path, label_path, img_size=224):
         # get all the image name and its label
         self.data_dict = {}
         with open(label_path, 'r') as f:
@@ -111,14 +119,16 @@ class MyDataFlowEval(DataFlow):
         self.image_path = image_path
         self.img_size = img_size
         self.Length = len(self.data_dict)
+        self.augmentors = fbresnet_augmentor(isTrain=False, target_shape=img_size)
 
     def get_data(self):
         for index, item in enumerate(self.img_name):
-            data = cv2.resize(cv2.imread(os.path.join(self.image_path, item)), self.img_size)
+            im = cv2.imread(os.path.join(self.image_path, item), cv2.IMREAD_COLOR)  
+            im = data_augmentation(im, self.augmentors)
             label = self.data_dict[item]
             yield {
                 'name': item, 
-                'data': np.expand_dims(np.array(data), axis=0),
+                'data': np.expand_dims(np.array(im), axis=0),
                 'label': np.array(label),
                 'epoch': (index+1) == self.Length
             }
--- eval.py
+++ eval.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
 import numpy as np
 import os
+import json
 from tensorpack import imgaug, dataset, ModelDesc, InputDesc
 from tensorpack.dataflow import (PrefetchDataZMQ, BatchData)
-from dataflow_input import MyDataFlow
+from dataflow_input import MyDataFlowEval
 import resnet_model
 from IPython import embed
 
-os.environ['CUDA_VISIBLE_DEVICES']= '1'
+os.environ['CUDA_VISIBLE_DEVICES']= '2'
 
 init_learning_rate = 0.1
 batch_size = 64
@@ -96,98 +97,42 @@ optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=mom
 # Batch norm requires update_ops to be added as a train_op dependency.
 update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 with tf.control_dependencies(update_ops):
-    train_op = optimizer.minimize(cost + l2_loss + C_loss)
+    train_op = optimizer.minimize(cost + l2_loss)
 
 correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_labels, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+values, indices = tf.nn.top_k(logits, 3)
+
 val_dir = '/data0/AIChallenger/ai_challenger_scene_validation_20170908/scene_validation_images_20170908/'
 annotations = '/data0/AIChallenger/ai_challenger_scene_validation_20170908/scene_validation_annotations_20170908.json'
 # a DataFlow you implement to produce [tensor1, tensor2, ..] lists from whatever sources:
-df = MyDataFlow(val_dir, annotations, is_training=False, batch_size=batch_size, img_size=image_size)
+df = MyDataFlowEval(val_dir, annotations, img_size=image_size)
 # start 3 processes to run the dataflow in parallel
-df = PrefetchDataZMQ(df, nr_proc=3)
+df = PrefetchDataZMQ(df, nr_proc=1)
 df.reset_state()
 scene_data_val = df.get_data()
-
-train_dir = '/data0/AIChallenger/ai_challenger_scene_train_20170904/scene_train_images_20170904/' 
-annotations = '/data0/AIChallenger/ai_challenger_scene_train_20170904/scene_train_annotations_20170904.json'
-# a DataFlow you implement to produce [tensor1, tensor2, ..] lists from whatever sources:
-df = MyDataFlow(train_dir, annotations, is_training=True, batch_size=batch_size, img_size=image_size)
-# start 3 processes to run the dataflow in parallel
-df = PrefetchDataZMQ(df, nr_proc=8)
-df.reset_state()
-scene_data = df.get_data()
 
 saver = tf.train.Saver(tf.global_variables())
 
 with tf.Session() as sess:
-    ckpt = tf.train.get_checkpoint_state('./model')
-    if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
-        print("loading checkpoint...")
-        saver.restore(sess, ckpt.model_checkpoint_path)
-    else:
-        sess.run(tf.global_variables_initializer())
+    ckpt = tf.train.get_checkpoint_state('./model_release')
+    print("loading checkpoint...")
+    saver.restore(sess, ckpt.model_checkpoint_path)
 
-    summary_writer = tf.summary.FileWriter('./logs', sess.graph)
-    
-    _x = x[:, :, :, ::-1]
-    tf.summary.image('x', _x, 4)
-    
-    summary_op = tf.summary.merge_all()
+    result = []
+    for it in scene_data_val:
+        temp_dict = {}
+        feed_dict = {x: it['data'], training_flag: False}
+        predictions = np.squeeze(sess.run(indices, feed_dict=feed_dict), axis=0)
+        temp_dict['image_id'] = it['name']
+        temp_dict['label_id'] = predictions.tolist()
+        result.append(temp_dict)
+        print('image %s is %d,%d,%d, label: %d' % (it['name'], predictions[0], predictions[1], predictions[2], it['label']))
+        if it['epoch']:
+            break
 
-    epoch_learning_rate = init_learning_rate
-    for epoch in range(1, total_epochs + 1):
-        if epoch % 30 == 0 :
-            epoch_learning_rate = epoch_learning_rate / 10
+    with open('submit.json', 'w') as f:
+        json.dump(result, f)
+        print('write result json, num is %d' % len(result))
 
-        train_acc = 0.0
-        train_loss = 0.0
-
-        for step in range(1, iteration + 1):
-            batch_data = next(scene_data)
-            batch_x = batch_data['data']
-            batch_y = batch_data['label']
-
-            train_feed_dict = {
-                x: batch_x,
-                label: batch_y,
-                learning_rate: epoch_learning_rate,
-                training_flag: True
-            }
-
-            _, batch_loss = sess.run([train_op, cost], feed_dict=train_feed_dict)
-            batch_acc = accuracy.eval(feed_dict=train_feed_dict)
-
-            print("epoch: %d/%d, iter: %d/%d, batch_loss: %.4f, batch_acc: %.4f \n" % (
-                epoch, total_epochs, step, iteration, batch_loss, batch_acc))
-
-            train_loss += batch_loss
-            train_acc += batch_acc
-
-            if step % 30 == 0 :
-                summary_str = sess.run(summary_op, feed_dict=train_feed_dict)
-                summary_writer.add_summary(summary=summary_str, global_step=epoch)
-                summary_writer.flush()
-
-
-        train_loss /= iteration # average loss
-        train_acc /= iteration # average accuracy
-
-        train_summary = tf.Summary(value=[tf.Summary.Value(tag='train_loss', simple_value=train_loss),
-                                          tf.Summary.Value(tag='train_accuracy', simple_value=train_acc)])
-
-        test_acc, test_loss, test_summary = Evaluate(sess)
-
-        summary_writer.add_summary(summary=train_summary, global_step=epoch)
-        summary_writer.add_summary(summary=test_summary, global_step=epoch)
-        summary_writer.flush()
-
-        line = "epoch: %d/%d, train_loss: %.4f, train_acc: %.4f, test_loss: %.4f, test_acc: %.4f \n" % (
-            epoch, total_epochs, train_loss, train_acc, test_loss, test_acc)
-        print(line)
-
-        with open('./logs/logs.txt', 'a') as f:
-            f.write(line)
-
-        saver.save(sess=sess, save_path='./model/model.ckpt')
